36c8a3bb9162bcfdf5d5adde7cfebfbd647a9199
violettape <my@violet-tape.net>
2014-12-18 18:12:38 +0300

    initial creation and adding nuget packages for TPL

---
+++ FreezableExample/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Runtime.InteropServices;
+using PostSharp.Patterns.Collections;
+using PostSharp.Patterns.Model;
+using PostSharp.Patterns.Threading;
+
+namespace FreezableExample {
+    internal class Program {
+        private static void Main(string[] args) {
+            var invoice = new Invoice();
+            ((IFreezable) invoice).Freeze();
+
+        }
+    }
+
+    [Freezable]
+    public class Invoice {
+        public readonly AdvisableCollection<InvoiceLine> Lines = new AdvisableCollection<InvoiceLine>();
+
+        public Customer Customer;
+    }
+
+    [Freezable]
+    public class InvoiceLine {
+        public Product Product;
+
+        public Invoice ParentInvoice { get; private set; }
+    }
+
+    public class Product {}
+
+    public class Customer {}
+}

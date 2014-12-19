f8a3631a830bd568fd01665158875a2c953631d9
VioletTape <my@violet-tape.net>
2014-12-19 11:35:26 +0300

    Examples for the Immutable case

--- ImmutableExample/Program.cs
+++ ImmutableExample/Program.cs
@@ -1,15 +1,49 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using PostSharp.Patterns.Collections;
+using PostSharp.Patterns.Model;
+using PostSharp.Patterns.Threading;
 
-namespace ImmutableExample
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+namespace ImmutableExample {
+    internal class Program {
+        private static void Main(string[] args) {
+            var invoice = new Invoice(123);
+    
+            Console.WriteLine(invoice.Items.First().Name);
+
+            Console.ReadLine();
         }
+    }
+
+    [Immutable]
+    public class Invoice  {
+
+        public long Id { get; set; }
+        public Invoice(long id)  {
+            Id = id;
+            Items = new AdvisableCollection<Item>();
+            Items.Add(new Item("widget"));
+        }
+
+        [Child]
+        public AdvisableCollection<Item> Items { get; set; }
+    }
+
+    [Immutable]
+    public class Document {
+        private long _id;
+
+        public Document(long id) {
+            _id = id;
+        }
+    }
+
+    [Immutable]
+    public class Item {
+        public Item(string name) {
+            Name = name;
+        }
+
+        public string Name { get; set; }
     }
 }

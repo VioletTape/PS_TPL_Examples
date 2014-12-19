a8a1ecd1601bf84d2f57d760be99cfec1c4968f3
VioletTape <my@violet-tape.net>
2014-12-19 10:28:31 +0300

    Change PostSharp version to the latest stable 4.0.39 Create example for Freezable

--- FreezableExample/Program.cs
+++ FreezableExample/Program.cs
@@ -1,4 +1,4 @@
-﻿using System.Runtime.InteropServices;
+﻿using System;
 using PostSharp.Patterns.Collections;
 using PostSharp.Patterns.Model;
 using PostSharp.Patterns.Threading;
@@ -7,26 +7,42 @@ namespace FreezableExample {
     internal class Program {
         private static void Main(string[] args) {
             var invoice = new Invoice();
-            ((IFreezable) invoice).Freeze();
+            invoice.Lines.Add(new InvoiceLine());
+            Console.WriteLine("New invoice line added");
+            invoice.Customer = new Customer {Name = "Smith"};
+            Console.WriteLine("New customer added");
 
+            ((IFreezable) invoice).Freeze();
+            Console.WriteLine(">> Invoice freezed");
+
+            invoice.Customer.Name = "Johnes";
+            Console.WriteLine("Customer name changed");
+
+            Console.ReadLine();
         }
     }
 
     [Freezable]
     public class Invoice {
+        [Child]
         public readonly AdvisableCollection<InvoiceLine> Lines = new AdvisableCollection<InvoiceLine>();
 
+        [Reference]
         public Customer Customer;
     }
 
     [Freezable]
     public class InvoiceLine {
+        [Reference]
         public Product Product;
 
+        [Parent]
         public Invoice ParentInvoice { get; private set; }
     }
 
     public class Product {}
 
-    public class Customer {}
+    public class Customer {
+        public string Name { get; set; }
+    }
 }

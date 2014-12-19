0b496b8b4115299b6c1c14f1bfe04cabc586e039
VioletTape <my@violet-tape.net>
2014-12-19 13:11:45 +0300

    Actor works properly

--- ActorExample/Program.cs
+++ ActorExample/Program.cs
@@ -1,15 +1,86 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+using PostSharp.Patterns.Threading;
 
-namespace ActorExample
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+namespace ActorExample {
+    internal class Program {
+        public Program() {}
+
+        private static void Main(string[] args) {
+            MainAsync().GetAwaiter().GetResult();
         }
+
+        private static async Task MainAsync() {
+            var averageCalculator = new AverageCalculator();
+
+            var observer = new SampleObserver(averageCalculator);
+            DataSources.Source1.Subscribe(observer);
+            DataSources.Source2.Subscribe(observer);
+            DataSources.Source3.Subscribe(observer);
+            DataSources.Source4.Subscribe(observer);
+
+            Console.WriteLine("started");
+
+            Console.ReadKey();
+            var average = await averageCalculator.GetAverage();
+
+            Console.WriteLine("Average: {0}", average);
+            Console.ReadLine();
+        }
+    }
+
+    public static class DataSources {
+        public static DataSource Source1 = new DataSource();
+        public static DataSource Source2 = new DataSource();
+        public static DataSource Source3 = new DataSource();
+        public static DataSource Source4 = new DataSource();
+    }
+
+    public class DataSource {
+        public void Subscribe(SampleObserver observer) {
+            new Task(() => Foo(observer), TaskCreationOptions.AttachedToParent).Start();
+        }
+
+        private void Foo(SampleObserver observer) {
+            for (int i = 1; i < 1000; i++) {
+                observer.OnNext(i);
+            }
+        }
+    }
+
+    [Actor]
+    public class AverageCalculator {
+        private float sum;
+        private int count;
+
+        public void AddSample(float n) {
+            count++;
+            sum += n;
+        }
+
+        [Reentrant]
+        public async Task<float> GetAverage() {
+            return sum / count;
+        }
+    }
+
+    public class SampleObserver : IObserver<float> {
+        private AverageCalculator calculator;
+
+        public SampleObserver(AverageCalculator averageCalculator) {
+            calculator = averageCalculator;
+        }
+
+
+        public void OnNext(float value) {
+            // Each of the data sources can call us from a different thread and concurrently.
+            // But we don't have to care since our calculator will enqueue method calls.
+            calculator.AddSample(value);
+        }
+
+        public void OnError(Exception error) {}
+
+        public void OnCompleted() {}
+
     }
 }
---
+++ ImmutableExample/ReadonlyCase.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+
+namespace ImmutableExample {
+    public class Invoice1 {
+        public readonly long _id;
+
+        public Invoice1(long id) {
+            SetIdentifier(id);
+        }
+
+        public void SetIdentifier(long id) {
+//            _id = id;
+        }
+    }
+
+
+    public class Invoice2 {
+        public readonly Customer _customer;
+
+        public Invoice2() {
+            _customer = new Customer();
+        }
+
+        public void Refresh() {
+            //valid but not immutable
+            _customer.Name = "Jim";
+            _customer.Phone = "555-123-9876";
+        }
+    }
+
+
+
+
+
+
+
+
+    public class Invoice3 {
+        public readonly IList<ItemX> _items;
+
+        public Invoice3() {
+            _items = new List<ItemX>();
+        }
+
+        public void Refresh() {
+            //will cause a compilation error
+//            _items = new List<Item>();
+
+            //valid but not immutable
+            _items.Add(new ItemX());
+            _items[0].Price = 3.50;
+            _items.RemoveAt(0);
+        }
+    }
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public class Customer {
+        public string Name { get; set; }
+        public string Phone { get; set; }
+    }
+
+    public class ItemX {
+        public double Price { get; set; }
+    }
+}
